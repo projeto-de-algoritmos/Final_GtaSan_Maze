c522224b7d316e6f867a3aef4f04159507b739f0
Victor Rayan Ferreira <victor.rayan378@gmail.com>
2022-09-13 11:31:34 -0300

    contando qtde de itens coletas interface feita

--- GtaSan_Maze/assets.py
+++ GtaSan_Maze/assets.py
@@ -1,8 +1,6 @@
 from generator import *
 from weapons import *
 
-
-
 def is_collide(x, y,player_rect,walls_collide_list):
     tmp_rect = player_rect.move(x, y)
     if tmp_rect.collidelist(walls_collide_list) == -1:
@@ -10,7 +8,6 @@ def is_collide(x, y,player_rect,walls_collide_list):
     return True
 
 # movimentação do cj
-
 def cj_move(maze):
     player_speed = 5
     player_img = pygame.image.load('images/cj_rindo.jpg').convert_alpha()
@@ -23,10 +20,40 @@ def cj_move(maze):
 
     return player_speed,player_img, player_rect, directions, direction, keys
 
+# Coletar item para a mochila
 def get_weapons(weapon_list,player_rect):
     for glock in weapon_list:
         if player_rect.collidepoint(glock.rect.center):
             glock.set_pos()
             return True
     return False
+
+# Desenhar weapons no menu
+def drawWeapons(maze):
+    glocks_img = pygame.image.load('images/glock_gta.jpg').convert_alpha()
+    glocks_img = pygame.transform.scale(glocks_img, (TILE - 2 * maze[0].thickness, TILE - 2 * maze[0].thickness))
+    glocks_rect = glocks_img.get_rect()
+    glocks_rect.topleft = WIDTH + 250, 500
+
+    eletrical_img = pygame.image.load('images/eletrical.jpg').convert_alpha()
+    eletrical_img = pygame.transform.scale(eletrical_img, (TILE - 2 * maze[0].thickness, TILE - 2 * maze[0].thickness))
+    eletrical_rect = eletrical_img.get_rect()
+    eletrical_rect.topleft = WIDTH + 250, 550
+
+    smg_img = pygame.image.load('images/metralha.jpg').convert_alpha()
+    smg_img = pygame.transform.scale(smg_img, (TILE - 2 * maze[0].thickness, TILE - 2 * maze[0].thickness))
+    smg_rect = smg_img.get_rect()
+    smg_rect.topleft = WIDTH + 250, 600
+
+    flower_img = pygame.image.load('images/flower.jpg').convert_alpha()
+    flower_img = pygame.transform.scale(flower_img, (TILE - 2 * maze[0].thickness, TILE - 2 * maze[0].thickness))
+    flower_rect = flower_img.get_rect()
+    flower_rect.topleft = WIDTH + 250, 650
+
+    binoculo_img = pygame.image.load('images/binoculo.jpg').convert_alpha() 
+    binoculo_img = pygame.transform.scale(binoculo_img, (TILE - 2 * maze[0].thickness, TILE - 2 * maze[0].thickness))
+    binoculo_rect = binoculo_img.get_rect()
+    binoculo_rect.topleft = WIDTH + 250, 700
+
+    return glocks_img, glocks_rect, eletrical_img, eletrical_rect, smg_img, smg_rect, flower_img, flower_rect, binoculo_img,binoculo_rect 
 
--- GtaSan_Maze/game.py
+++ GtaSan_Maze/game.py
@@ -12,11 +12,14 @@ clock = pygame.time.Clock()
 game_background = pygame.image.load('images/gamebackground.jpg').convert()
 gta_background = pygame.image.load('images/logogta.jpg').convert()
 text_font = pygame.font.SysFont('Impact', 80)
+weapons_font = pygame.font.SysFont('Impact', 30)
 
 maze = gamemap()
 
 # player settings
 player_speed,player_img, player_rect, directions, direction, keys = cj_move(maze)
+glocks_img, glocks_rect, eletrical_img, eletrical_rect, smg_img, smg_rect, flower_img, flower_rect, binoculo_img,binoculo_rect = drawWeapons(maze)
+glock_qtde , eletrical_qtde, smg_qtde, flower_qtde, binoculo_qtde = 0, 0, 0, 0, 0
 
 # Respawn weapons no mapa
 Glock_list = [Glock(game_surface) for i in range(3)]
@@ -57,22 +60,27 @@ while True:
     if get_weapons(Glock_list,player_rect):
         FPS += 1
         time += 5
+        glock_qtde += 1
 
     if get_weapons(Smg_list,player_rect):
         FPS += 1  
         time += 11
+        smg_qtde += 1
 
     if get_weapons(Flower_list,player_rect):
         FPS += 1
         time += 7
+        flower_qtde += 1
 
     if get_weapons(Binoculo_list,player_rect):
         FPS += 1
         time += 8
+        binoculo_qtde += 1
 
     if get_weapons(Eletrical_list,player_rect):
         FPS += 1 
-        time += 9                
+        time += 9  
+        eletrical_qtde += 1          
         
     game_surface.blit(player_img, player_rect)
 
@@ -86,8 +94,21 @@ while True:
 
     [binoculo.draw() for binoculo in Binoculo_list]
 
-    surface.blit(text_font.render('TIME', True, pygame.Color('grey'), True), (WIDTH + 150 , 400))
-    surface.blit(text_font.render(f'{time}', True, pygame.Color('grey')), (WIDTH + 350, 400))
+    surface.blit(text_font.render('TIME', True, pygame.Color('grey'), True), (WIDTH + 150 , 370))
+    surface.blit(text_font.render(f'{time}', True, pygame.Color('grey')), (WIDTH + 350, 370))
 
+    surface.blit(glocks_img, glocks_rect)
+    surface.blit(eletrical_img, eletrical_rect)
+    surface.blit(smg_img, smg_rect)
+    surface.blit(flower_img, flower_rect)
+    surface.blit(binoculo_img, binoculo_rect)
+    
+    surface.blit(weapons_font.render(f'-  {glock_qtde}x', True, pygame.Color('grey'), True), (WIDTH + 300, 500))
+    surface.blit(weapons_font.render(f'-  {eletrical_qtde}x', True, pygame.Color('grey'), True), (WIDTH + 300, 550))
+    surface.blit(weapons_font.render(f'-  {smg_qtde}x', True, pygame.Color('grey'), True), (WIDTH + 300, 600))
+    surface.blit(weapons_font.render(f'-  {flower_qtde}x', True, pygame.Color('grey'), True), (WIDTH + 300, 650))
+    surface.blit(weapons_font.render(f'-  {binoculo_qtde}x', True, pygame.Color('grey'), True), (WIDTH + 300, 700))
+    
+    
     pygame.display.flip()
     clock.tick(FPS)
