72670917fc7d0709756573fdc5c0efa01fdaa943
Victor Rayan Ferreira <victor.rayan378@gmail.com>
2022-09-13 01:29:15 -0300

    add background do jogo e organizando game.py

--- GtaSan_Maze/game.py
+++ GtaSan_Maze/game.py
@@ -7,14 +7,21 @@ game_surface = pygame.Surface(RES)
 surface = pygame.display.set_mode((WIDTH + 600, HEIGHT))
 clock = pygame.time.Clock()
 
+game_background = pygame.image.load('images/gamebackground.jpg').convert()
+gta_background = pygame.image.load('images/logogta.jpg').convert()
 
 maze = gamemap()
 
 
 while True:
     
+    surface.blit(gta_background, (1190, 0))
     surface.blit(game_surface, (0, 0))
-    
+    game_surface.blit(game_background, (0, 0))
+
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            exit()
     # draw maze
     [cell.draw(game_surface) for cell in maze]
 
